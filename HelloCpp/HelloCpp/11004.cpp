74f0fea2999400a6a428f5052cf6507332b0ab05
Amenable <duswo0624@naver.com>
2021-10-01 23:52:35 +0900

    11004

---
+++ HelloCpp/HelloCpp/11004.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//int main() {
+//	int n, k;
+//	vector<int> data;
+//	cin >> n >> k;
+//
+//	for (int i = 0; i < n; i++) {
+//		int a;
+//		scanf("%d", &a);
+//		data.push_back(a);
+//	}
+//	sort(data.begin(), data.end());
+//	cout << data[k - 1];
+//
+//}
