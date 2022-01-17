a58d35d564cd4f5a80afa23fa6d9ed1ad56807fd
Amenable <duswo0624@naver.com>
2022-01-17 10:32:05 +0900

    Assignment11

---
+++ mainStackException2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Stack.h"
+#include "StackException.h"
+
+//int main(){
+//    try{
+//    Stack<char> stack(1);
+//    stack.push('a');
+//    stack.pop(); stack.pop();
+//    stack.print();
+//    }catch(StackException e){
+//        std::cout << e.what() << std::endl;
+//    }
+//
+//
+//}
+
+//int main(){
+//    try{
+//    Stack<char> stack(1);
+//    stack.push('a');
+//    stack.push('b');
+//
+//    stack.print();
+//    }catch(StackException e){
+//        std::cout << e.what() << std::endl;
+//    }
+//}
+
+//int main(){
+//    try{
+//    Stack<int> stack(1);
+//    stack.push(1);
+//    stack.pop();
+//    stack.push(2);
+//
+//    stack.print();
+//    stack.pop();
+//    }catch(StackException e){
+//        std::cout << e.what() << std::endl;
+//    }
+//}
+
+int main(){
+    try{
+    Stack<int> stack(2);
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    stack.pop();
+    stack.push(3);
+
+    stack.print();
+    }catch(StackException e){
+        std::cout << e.what() << std::endl;
+    }
+}
+
